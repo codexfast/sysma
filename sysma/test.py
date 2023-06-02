f6822da4cca5091a861ae5e502f06409c1692231
Gilberto <codexfast@gmail.com>
2023-06-02 13:16:11 -0300

    some changes

--- sysma/test.py
+++ sysma/test.py
@@ -1,5 +1,17 @@
-from datetime import datetime
+from config import DB_ENGINE, SysplData
+from sqlalchemy.orm import Session
+from controllers.functionalities.tools import plate_convert
 
-date = datetime.strptime("17/05/2023", "%d/%m/%Y")
+with Session(DB_ENGINE) as session:
+    failed = \
+        session.query(SysplData).filter(
+            SysplData.history_id == 1, SysplData.failed == True
+        ).all()
+    
+    for f in failed:
+        print(i.finaceira_nome)
 
-print(date.date())
+    # for i in failed:
+    #     i.finaceira_nome = "teste"
+
+    # session.commit()
